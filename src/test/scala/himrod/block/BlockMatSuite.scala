36dc4e293f820b696158dd576e66f8fbfc6fc75f
Michael Hynes <mbhynes@uwaterloo.ca>
2014-12-29 15:48:55 -0500

    More changes... tests changed too.

--- src/test/scala/himrod/block/BlockMatSuite.scala
+++ src/test/scala/himrod/block/BlockMatSuite.scala
@@ -32,48 +32,47 @@ class BlockMatSuite extends FunSuite with LocalSparkContext
 	val mat_fin: String = "src/test/scala/himrod/block/mat_data"
 	val delim: String = ",";
 
-	/*test("Instantiate BlockVec from file")*/
-	/*{*/
-	/*	BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vec_bsize).print();*/
-	/*	BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vecSize).print();*/
-	/*	BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,1).print();*/
-	/*}*/
+	test("Instantiate BlockVec from file")
+	{
+		println("Instantiate BlockVec from file...")
+		BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vec_bsize).print();
+		println("Instantiate BlockVec from file...")
+		BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vecSize).print();
+		println("Instantiate BlockVec from file...")
+		BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,1).print();
+	}
 
-	/*test("Generate Random BlockVec")*/
-	/*{*/
-	/*	//test rand generation*/
-	/*	val test1 = BlockVec.rand(sc,vecSize,vec_bsize);*/
-	/*	test1.print();*/
-	/*}*/
+	test("Generate Random BlockVec")
+	{
+		//test rand generation
+		println("Generate Random BlockVec")
+		val test1 = BlockVec.rand(sc,vecSize,vec_bsize);
+		test1.print();
+	}
 
-	/*test("BlockVec Scalar Addition/Multiplication")*/
-	/*{*/
-	/*	val test = BlockVec.rand(sc,vecSize,vec_bsize);*/
-	/*	test.print();*/
-	/*	val a: Double = 10;*/
-	/*	(test+a).print();*/
-	/*	(test*a).print();*/
-	/*}*/
+	test("BlockVec Scalar Addition/Multiplication")
+	{
+		val test = BlockVec.rand(sc,vecSize,vec_bsize);
+		val a: Double = 10;
+		println("Vec scalar addition");
+		(test+a).print();
+		println("Vec scalar mult");
+		(test*10.0*a).print();
+	}
 
-	/*test("BlockVec (Random) Addition/Multiplication")*/
-	/*{*/
-	/*	val a: Double = 2;*/
-	/*	val b: Double = 10;*/
-	/*	val test1 = BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vec_bsize);*/
-	/*	val test2 = BlockVec.zeros(sc,vecSize,vec_bsize)+b;*/
-	/*	test1.print()*/
-	/*	test2.print()*/
-	/*	try*/
-	/*	{*/
-	/*		val test3 = test1 + test2;*/
-	/*		println("Success: ");*/
-	/*		test3.print()*/
-	/*	}*/
-	/*	catch*/
-	/*	{*/
-	/*		case ex: BlockVecSizeMismatchException => errorMessage(ex);*/
-	/*	}*/
-	/*}*/
+	test("Vector-Vector Element-Wise Addition")
+	{
+		val a: Double = 2;
+		val b: Double = 10;
+		val test1 = BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vec_bsize);
+		/*val test1 = BlockVec.zeros(sc,vecSize,vec_bsize)+a;*/
+		val test2 = BlockVec.ones(sc,vecSize,vec_bsize);
+		val test3 = (test1 + test2);
+		println("Vector-Vector Element-Wise Addition")
+		test1.print();
+		test2.print();
+		test3.print();
+	}
 
 	test("Vector-Vector Element-Wise Multiplication")
 	{
@@ -83,6 +82,7 @@ class BlockMatSuite extends FunSuite with LocalSparkContext
 		/*val test1 = BlockVec.zeros(sc,vecSize,vec_bsize)+a;*/
 		val test2 = BlockVec.ones(sc,vecSize,vec_bsize);
 		val test3 = (test1 * test2);
+		println("Vector-Vector Element-Wise Multiplication")
 		test1.print();
 		test2.print();
 		test3.print();
@@ -92,7 +92,7 @@ class BlockMatSuite extends FunSuite with LocalSparkContext
 	{
 		val a: Double = 2;
 		val b: Double = 10;
-		val test1 = BlockVec.fromTextFile(sc,vec_fin,delim,vecSize,vec_bsize);
+		val test1 = BlockVec.ones(sc,vecSize,vec_bsize);
 		/*val test1 = BlockVec.zeros(sc,vecSize,vec_bsize)+a;*/
 		val test2 = BlockVec.ones(sc,vecSize,vec_bsize);
 		/*test1.print()*/
@@ -103,23 +103,25 @@ class BlockMatSuite extends FunSuite with LocalSparkContext
 	test("Matrix-Vector Multiplication BlockMat")
 	{
 		//test rand generation
-		println("M-V Multiplication");
 		val mat = BlockMat.fill(sc,matSize,bsize,1);
 		val vec = BlockVec.fill(sc,vecSize,vec_bsize,1);
 		val result = mat.multiply(vec);
 
+		println("M-V Multiplication");
 		mat.print();
 		vec.print();
 		result.print();
+		assert(result === BlockVec.fill(sc,vecSize,vec_bsize,16.0))
 	}
+
 	test("Vector-Matrix Multiplication BlockMat")
 	{
 		//test rand generation
-		println("V-M Multiplication");
 		val mat = BlockMat.ones(sc,matSize,bsize);
 		val vec = BlockVec.ones(sc,vecSize,vec_bsize);
 		val result = vec.multiply(mat);
 
+		println("V-M Multiplication");
 		mat.print();
 		vec.print();
 		result.print();
@@ -141,82 +143,82 @@ class BlockMatSuite extends FunSuite with LocalSparkContext
 	}
 
 	
-	/*test("Instantiate BlockMat from file")*/
-	/*{*/
-	/*	val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);*/
-	/*	test1.print();*/
+	test("Instantiate BlockMat from file")
+	{
+		val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);
+		test1.print();
 
-	/*	val test2 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(1,1));*/
-	/*	test2.print();*/
+		val test2 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(1,1));
+		test2.print();
 
-	/*	val test3 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(4,1));*/
-	/*	test3.print();*/
+		val test3 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(4,1));
+		test3.print();
 
-	/*	val test4 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(1,4));*/
-	/*	test4.print();*/
-	/*}*/
+		val test4 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(1,4));
+		test4.print();
+	}
 
-	/*test("Generate Random BlockMat")*/
-	/*{*/
-	/*	//test rand generation*/
-	/*	val test1 = BlockMat.rand(sc,matSize,bsize);*/
-	/*	test1.print();*/
-	/*}*/
+	test("Generate Random BlockMat")
+	{
+		//test rand generation
+		val test1 = BlockMat.rand(sc,matSize,bsize);
+		test1.print();
+	}
 
-	/*test("BlockMat Scalar Addition/Multiplication")*/
-	/*{*/
-	/*	val test = BlockMat.rand(sc,matSize,bsize);*/
-	/*	test.print();*/
-	/*	val a: Double = 10;*/
-	/*	(test+a).print();*/
-	/*	(test*a).print();*/
-	/*}*/
+	test("BlockMat Scalar Addition/Multiplication")
+	{
+		val test = BlockMat.rand(sc,matSize,bsize);
+		test.print();
+		val a: Double = 10;
+		(test+a).print();
+		(test*a).print();
+	}
 
-	/*test("BlockMat (Random) Addition/Multiplication")*/
-	/*{*/
-	/*	val a: Double = 2;*/
-	/*	val b: Double = 10;*/
-	/*	val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(4,4));*/
-	/*	val test2 = BlockMat.zeros(sc,matSize,BlockSize(4,4))+b;*/
-	/*	test1.print()*/
-	/*	test2.print()*/
-	/*	try*/
-	/*	{*/
-	/*		val test3 = test1 + test2;*/
-	/*		println("Success: ");*/
-	/*		test3.print()*/
-	/*	}*/
-	/*	catch*/
-	/*	{*/
-	/*		case ex: BlockMatSizeMismatchException => errorMessage(ex);*/
-	/*	}*/
-	/*}*/
+	test("BlockMat (Random) Addition/Multiplication")
+	{
+		val a: Double = 2;
+		val b: Double = 10;
+		val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,BlockSize(4,4));
+		val test2 = BlockMat.zeros(sc,matSize,BlockSize(4,4))+b;
+		test1.print()
+		test2.print()
+		try
+		{
+			val test3 = test1 + test2;
+			println("Success: ");
+			test3.print()
+		}
+		catch
+		{
+			case ex: BlockMatSizeMismatchException => errorMessage(ex);
+		}
+	}
 
 	/*test("BlockMat EYE")*/
 	/*{*/
 	/*	BlockMat.eye(sc,matSize,bsize).print();*/
 	/*}*/
 
-	/*test("Matrix-Matrix Multiplication")*/
-	/*{*/
-	/*	val a: Double = 2;*/
-	/*	val b: Double = 10;*/
-	/*	val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);*/
-	/*	/*val test1 = BlockMat.zeros(sc,matSize,bsize)+a;*/*/
-	/*	val test2 = BlockMat.zeros(sc,matSize,bsize)+b;*/
-	/*	(test1 * test2).print*/
-	/*}*/
-	/*test("Matrix-Vector Multiplication")*/
-	/*{*/
-	/*	val a: Double = 2;*/
-	/*	val b: Double = 10;*/
-	/*	val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);*/
-	/*	/*val test1 = BlockMat.zeros(sc,matSize,bsize)+a;*/*/
-	/*	val test2 = BlockMat.zeros(sc,BlockSize(4,1),BlockSize(2,1))+b;*/
-	/*	test1.print;*/
-	/*	test2.print;*/
-	/*	(test1 * test2).print;*/
-	/*}*/
+	test("Matrix-Matrix Multiplication")
+	{
+		val a: Double = 2;
+		val b: Double = 10;
+		val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);
+		/*val test1 = BlockMat.zeros(sc,matSize,bsize)+a;*/
+		val test2 = BlockMat.zeros(sc,matSize,bsize)+b;
+		(test1 * test2).print
+	}
+	test("Matrix-Vector Multiplication")
+	{
+		val a: Double = 2;
+		val b: Double = 10;
+		val test1 = BlockMat.fromTextFile(sc,mat_fin,delim,matSize,bsize);
+		/*val test1 = BlockMat.zeros(sc,matSize,bsize)+a;*/
+		val test2 = BlockMat.zeros(sc,BlockSize(4,1),BlockSize(2,1))+b;
+		test1.print;
+		test2.print;
+		(test1 * test2).print;
+	}
 
 	/*test("Matrix of Different BlockSize from File")*/
 	/*{*/
